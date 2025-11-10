26651452141401a443ef99ebd27bc6111ef47077
Natalia Repkina <pn.88@mail.ru>
2025-11-10 12:57:49 +0300

    fix: fix interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { routes } from './app.routes';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideEffects()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideStore(),
+  provideEffects(),
+  provideHttpClient(
+    withInterceptors([apiInterceptor])
+  )
+  ]
 };
