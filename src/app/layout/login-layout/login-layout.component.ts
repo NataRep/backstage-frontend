aa2ff396d8989104bdab59d000e0b28fed12737d
Natalia Repkina <pn.88@mail.ru>
2025-11-17 13:19:31 +0300

    feat: implement person service

--- src/app/layout/login-layout/login-layout.component.ts
+++ src/app/layout/login-layout/login-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterModule],
   templateUrl: './login-layout.component.html',
   styleUrl: './login-layout.component.scss'
 })
