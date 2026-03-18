6a509c993637514d67aa44c2c05ebb7337b5ad0e
Natalia Repkina <pn.88@mail.ru>
2026-03-18 11:30:23 +0300

    feat: add filling team calendar with data

--- src/app/core/models/interfaces/employee.models.ts
+++ src/app/core/models/interfaces/employee.models.ts
@@ -1,11 +1,10 @@
-import { Timestamp } from "rxjs";
 import { AccessLevel } from "../enums/auth.enums";
 import { Role } from "../enums/employee.enums";
 import { Person } from "./person.model";
 
 export interface WorkerBase {
   roles: Role[];
-  availability: Timestamp<string>[];
+  availability: CustomTimestamp[];
   isActive: boolean;
   accessLevel: AccessLevel;
 }
@@ -18,4 +17,9 @@ export interface Worker extends WorkerBase {
 export interface EmployeeProfile {
   person: Person | null;
   worker: Worker | null;
+}
+
+export interface CustomTimestamp {
+  seconds: number;
+  nanoseconds: number;
 }
--- src/app/features/team/team-calendar/team-calendar.component.ts
+++ src/app/features/team/team-calendar/team-calendar.component.ts
@@ -32,7 +32,8 @@ export class TeamCalendarComponent extends BaseTableDirective<EmployeeProfile> i
   @HostListener('window:resize')
   onResize() {
     this.calculatePageSize();
-  }
+  };
+
   // 1. Dependency Injection
   private readonly store = inject(Store);
   private readonly actions = inject(Actions);
@@ -53,6 +54,7 @@ export class TeamCalendarComponent extends BaseTableDirective<EmployeeProfile> i
   readonly canEdit = this.store.selectSignal(selectCanEdit);
 
   // 5. Local State (Signals)
+  hoverDay = signal<number | null>(null);
   selectedEmployee = signal<EmployeeProfile | null>(null);
   selectedRole = signal<Role | 'all'>((this.route.snapshot.queryParamMap.get('role') as Role) || 'all');
   selectedPeriod = signal({
@@ -61,6 +63,29 @@ export class TeamCalendarComponent extends BaseTableDirective<EmployeeProfile> i
   });
 
   // 6. Computed Properties
+  readonly availabilityMap = computed(() => {
+    const { month, year } = this.selectedPeriod();
+    const employees = this.filteredEmployees();
+    const map = new Map<string, Set<number>>();
+
+    employees.forEach(emp => {
+      const unavailableDays = new Set<number>();
+
+      emp.worker?.availability?.forEach(timestamp => {
+        const date = new Date(timestamp.seconds * 1000);
+
+        // Проверяем, попадает ли дата в текущий выбранный месяц и год
+        if (date.getMonth() === month && date.getFullYear() === year) {
+          unavailableDays.add(date.getDate());
+        }
+      });
+
+      map.set(emp.person?.personId!, unavailableDays);
+    });
+
+    return map;
+  });
+
   readonly calendarDays = computed(() =>
     Array.from({ length: getDaysInMonth(this.selectedPeriod().month, this.selectedPeriod().year) }, (_, i) => i + 1)
   );
@@ -96,6 +121,8 @@ export class TeamCalendarComponent extends BaseTableDirective<EmployeeProfile> i
       this.store.dispatch(getAllEmployeesAction());
     }
     this.calculatePageSize();
+
+    console.log("currentUser", this.currentUser())
   }
 
   // 8. Event Handlers & Business Logic
@@ -127,5 +154,12 @@ export class TeamCalendarComponent extends BaseTableDirective<EmployeeProfile> i
 
     // Устанавливаем минимум 5 строк, чтобы таблица не схлопнулась совсем
     this.dynamicPageSize.set(Math.max(calculatedRows, 5));
+  }
+
+  private readonly todayMidnight = new Date().setHours(0, 0, 0, 0);
+  isPastDay(day: number): boolean {
+    const { month, year } = this.selectedPeriod();
+    const dateToCheck = new Date(year, month, day).getTime();
+    return dateToCheck < this.todayMidnight;
   }
 }
